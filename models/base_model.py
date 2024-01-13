06c9d91f1478e2c5c8122df84f9dd863ebfda559
hady444 <hadyhassan2312@gmail.com.com>
2024-01-13 17:06:11 +0000

    Task 1 v2

--- models/base_model.py
+++ models/base_model.py
@@ -9,14 +9,14 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self):
         return ("[{}], ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         dic = dict()
