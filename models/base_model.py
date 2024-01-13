3d5f687db0c29d616c0d40152ad0e1e38ba1c35f
hady444 <hadyhassan2312@gmail.com.com>
2024-01-13 17:25:49 +0000

    Task 1 v3

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
         self.updated_at = datetime.utcnow()
 
     def __str__(self):
-        return ("[{}], ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         self.updated_at = datetime.utcnow()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,19 +3,39 @@
 unittests of basemodel
 """
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
     """
     Test attributes
     """
     def setUp(self):
-        pass
-    def test_object_created(self):
-        my_mod = BaseModel()
-        my_mod.name = "first model"
-        my_mod.my_number = 55
-        self.assertEqual([my_mod.name, my_mod.my_number], ["first model", 55])
-    def test_date_time
+        self.my_model = BaseModel()
+        self.my_model.name = "My First Model"
+        self.my_model.my_number = 89
+
+    def test_str_method(self):
+        expected_output = "[BaseModel] ({}) {}".format(self.my_model.id, self.my_model.__dict__)
+        self.assertEqual(str(self.my_model), expected_output)
+
+    def test_save_method(self):
+        initial_updated_at = self.my_model.updated_at
+        self.my_model.save()
+        self.assertNotEqual(self.my_model.updated_at, initial_updated_at)
+
+    def test_to_dict_method(self):
+        my_model_json = self.my_model.to_dict()
+
+        self.assertIn('__class__', my_model_json)
+        self.assertEqual(my_model_json['__class__'], 'BaseModel')
+
+        for key, value in self.my_model.__dict__.items():
+            if isinstance(value, datetime):
+                self.assertEqual(my_model_json[key], value.isoformat())
+            else:
+                self.assertEqual(my_model_json[key], value)
+
     if __name__ == '__main__':
         unittest.main()
