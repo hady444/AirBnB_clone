598aaedc886d3488f18c17d3454bf63757edbf70
hady444 <hadyhassan2312@gmail.com.com>
2024-01-14 08:44:48 +0000

    Task 4 v4

--- console.py
+++ console.py
@@ -8,13 +8,17 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_EOF(self, arg):
-        """EOF command to exit the program with ctrl+d\n"""
+        """EOF command to exit the program with ctrl+d"""
         print("")
         return True
 
     def do_quit(self, arg):
-        """Quit command to exit the program\n"""
+        """Quit command to exit the program"""
         return True
+
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
